66440d6d95e788420e95b34fc024e3fef05e2adf
Bill Wilder <codingoutloud@gmail.com>
2018-01-15 10:30:58 -0500

    baseline

---
+++ get.sh
@@ -0,0 +1,9 @@
+URL=https://opstoolbox.azurewebsites.net/api/HttpTriggerCSharp/name/codingoutloud.com?code=Z/CXmZBkGbni/k5D9PpnNHokfa2QxpWiuFwr7L08TqwTLS83dqw1DQ==
+
+echo "URL = $URL"
+
+curl $URL
+
+echo ""
+
+
